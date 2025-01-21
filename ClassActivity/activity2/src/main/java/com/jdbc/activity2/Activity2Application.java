0a983e631d127f9d68171f6818e2bed3d892dbd8
Ramees <ramees.kr@outlook.com>
2025-01-21 10:08:11 -0500

    Created Activity2

---
+++ ClassActivity/activity2/src/main/java/com/jdbc/activity2/Activity2Application.java
@@ -0,0 +1,13 @@
+package com.jdbc.activity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activity2Application.class, args);
+	}
+
+}
---
+++ ClassActivity/activity2/src/test/java/com/jdbc/activity2/Activity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbc.activity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
