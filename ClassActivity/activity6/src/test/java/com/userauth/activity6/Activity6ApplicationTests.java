7569fa7c5bcbe958799af2aef9d7e6a25b710790
Ramees <ramees.kr@outlook.com>
2025-02-18 11:14:23 -0500

    Activity6

---
+++ ClassActivity/activity6/src/test/java/com/userauth/activity6/Activity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userauth.activity6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
